47044bda5695b1527260188330056fccb3b2315e
Sachin Singh <contactsachinsingh@gmail.com>
2022-04-02 13:22:27 +0530

    Added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/'
+    publicPath: ''
   },
   mode: 'none',
   module: {
@@ -50,6 +51,15 @@ module.exports = {
         '**/*',
         path.join(process.cwd(), 'build/**/*')
       ]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      meta: {
+        'X-UA-Compatible': {
+          'http-equiv': 'X-UA-Compatible',
+          content: 'IE=edge'
+        }
+      }
     })
   ]
 };
