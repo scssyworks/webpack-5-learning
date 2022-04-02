615401edc036bf382cd8cf428029ccf62859aae6
Sachin Singh <contactsachinsingh@gmail.com>
2022-04-02 16:27:31 +0530

    Added split chunks

--- src/hello-world.js
+++ src/hello-world.js
@@ -2,6 +2,7 @@
 import { upperFirst } from 'lodash';
 import Heading from './components/heading/heading';
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import './index.css';
 
 new Heading().render(upperFirst('hello world'));
 new HelloWorldButton().render();
--- src/kiwi.js
+++ src/kiwi.js
@@ -2,6 +2,7 @@
 import { upperFirst } from 'lodash';
 import Heading from './components/heading/heading';
 import KiwiImage from './components/kiwi-image/kiwi-image';
+import './index.css';
 
 new Heading().render(upperFirst('kiwi'));
 new KiwiImage().render();
